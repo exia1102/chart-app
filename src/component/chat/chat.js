e7c9f70b2c44791e08beef161be07ca052a69f41
exia1102 <41196861+exia1102@users.noreply.github.com>
2018-09-24 22:50:11 -0400

    Add files via upload

--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,13 +1,70 @@
 import React from 'react';
+import io from 'socket.io-client'
+import {InputItem,List} from 'antd-mobile';
+import {connect} from 'react-redux';
+import {getMsgList} from '../../redux/chat.redux';
+
+const socket = io('ws://localhost:9093');
 
 
+@connect(
+    state=>state.chat,
+    {getMsgList}
+)
 class Chat extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            text:'',
+            msg:[]
+        }
+    }
 
+    componentDidMount(){
+        // socket.on('recvmsg',(data)=>{
+        //     this.setState({
+        //         msg:[...this.state.msg,data.text]
+        //     })
+        // });
+        this.props.getMsgList();
+    }
+
+    handleSubmit(){
+        socket.emit('sendmsg',{text:this.state.text})
+        this.setState({
+            text:''
+        })
+    }
 
     render(){
         console.log(this.props.match);
         return(
-            <div>{this.props.match.params.user}</div>
+                <div>
+
+                    {this.state.msg.map((v)=>{
+                        return <p key={v}>{v}</p>
+                    })}
+
+                    <div className="stick-footer">
+                        <List>
+                            <InputItem
+                                placeholder='enter'
+                                value={this.state.text}
+                                onChange={(v)=>{
+                                    this.setState({
+                                        text:v
+                                    })
+                                }}
+                                extra={<span onClick={()=>this.handleSubmit()}>send</span>}
+                            >message</InputItem>
+                        </List>
+
+                    </div>
+                </div>
+
+
+
+
         )
     }
 
